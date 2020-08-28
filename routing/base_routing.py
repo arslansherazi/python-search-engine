b299acb1ca0fccb13df3afaa6520e958974f40fd
arslansherazi <arslanhaidersherazi@hotmail.com>
2020-08-28 19:27:40 +0500

    architecture

--- routing/base_routing.py
+++ routing/base_routing.py
@@ -1,18 +1,24 @@
-from app import api
+from flask_restful import Api
+
 from common.constants import API_BASE_END_POINT
 
 
 class BaseRouting(object):
-    routing_collection = {}
     api_version = None
+
+    def __init__(self, app):
+        self.routing_collection = {}
+        self.api = Api(app)
 
     def set_routing_collection(self):
         pass
 
     def set_routing(self):
         for api_endpoint, api_class in self.routing_collection.items():
-            api_path = 'v{version}/{end_point}'.format(version=self.api_version, end_point=api_endpoint)
-            api.add_resource(api_class, API_BASE_END_POINT, endpoint=api_path)
+            api_path = '{base_url}/v{version}/{endpoint}'.format(
+                base_url=API_BASE_END_POINT, version=self.api_version, endpoint=api_endpoint
+            )
+            self.api.add_resource(api_class, api_path)
 
     def map_routing(self):
         self.set_routing_collection()
